3c678b3b053f9d26625cd1671c619bf3ccaf3f5c
孟令通 <ak-7755@qq.com>
2018-01-08 15:13:49 +0800

    update demo

---
+++ BlogDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogDemo
+//
+//  Created by 孟令通 on 2018/1/6.
+//  Copyright © 2018年 LryMlt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogDemo
+//
+//  Created by 孟令通 on 2018/1/6.
+//  Copyright © 2018年 LryMlt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
