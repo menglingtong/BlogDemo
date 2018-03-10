d8e2921a92de880b04390d34bf76d91a2cf7eae6
孟令通 <ak-7755@qq.com>
2018-03-10 15:27:18 +0800

    NSString hash

--- BlogDemo/LTCategory/NSString+Additions.h
+++ BlogDemo/LTCategory/NSString+Additions.h
@@ -14,4 +14,13 @@
 + (BOOL)regularForPsw:(NSString *)psw;
 + (BOOL)regulatForBankCardNumber:(NSString *)cardNumber;
 
+@property (readonly) NSString *md5String;
+@property (readonly) NSString *sha1String;
+@property (readonly) NSString *sha256String;
+@property (readonly) NSString *sha512String;
+
+- (NSString *)hmacSHA1StringWithKey:(NSString *)key;
+- (NSString *)hmacSHA256StringWithKey:(NSString *)key;
+- (NSString *)hmacSHA512StringWithKey:(NSString *)key;
+
 @end
